45d0974c77bd97da1c486c825df497610a6e10ae
Dina <tumanita@gmail.com>
2023-07-13 13:47:15 -0600

    Adding Lab 1 folder

---
+++ Lab1/Lab1.X/oscilador.h
@@ -0,0 +1,12 @@
+#ifndef OSCILADOR_H
+#define OSCILADOR_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include <stdint.h>
+
+void configOsc(uint16_t frec);
+
+#endif //oscilador
+
+
+
